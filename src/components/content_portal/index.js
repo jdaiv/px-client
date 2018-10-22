95b898bbbc67bd9839cac15c01ad8176ff8a3e38
John Aivaliotis <johnaivaliotis@protonmail>
2018-10-22 20:36:52 +1100

    Added some name tags.

--- src/components/content_portal/index.js
+++ src/components/content_portal/index.js
@@ -1,9 +1,10 @@
 import { h, Component } from 'preact'
 import { inject, observer } from 'mobx-preact'
 
+import EventManager from '../../services/EventManager'
 import Engine from '../../engine/Engine'
-import ActFireworks from './ActFireworks'
-import ActTictactoe from './ActTictactoe'
+// import ActFireworks from './ActFireworks'
+// import ActTictactoe from './ActTictactoe'
 // import BootlegHearthstone from '../../activities/bootleg_hearthstone'
 
 import style from './style'
@@ -41,7 +42,55 @@ export default class ContentPortal extends Component {
         // }
         // return <div class={style.portal}>{content}</div>
 
-        return <div class={style.portal} />
+        return (
+            <div class={style.portal}>
+                <Overlay engine={this.engine} />
+            </div>
+        )
+    }
+
+}
+
+class Overlay extends Component {
+
+    state = {
+        players: []
+    }
+
+    componentDidMount() {
+        EventManager.subscribe('ws/chat/update_room', 'overlay', ({ action, data }) => {
+            let players = []
+            for (let key in data.state.players) {
+                players.push({
+                    key,
+                    data: data.state.players[key]
+                })
+            }
+            this.setState({
+                players
+            })
+        })
+    }
+
+    componentWillUnmount() {
+        EventManager.unsubscribe('ws/chat/update_room', 'overlay')
+    }
+
+    render() {
+        if (!this.base) return <div />
+        const rect = this.base.getBoundingClientRect()
+        const cX = rect.width / 2
+        const cY = rect.height / 2
+        let bits = this.state.players.map(p => {
+            const left = cX + p.data.x * 2 + p.data.z * 2 - 50
+            const top = cY + p.data.y * 2 + p.data.z * 2
+            return <div class={style.bit} style={{ left, top }}>{p.key}</div>
+        })
+        return (
+            <div class={style.overlay}>
+                {bits}
+            </div>
+        )
     }
 
 }
