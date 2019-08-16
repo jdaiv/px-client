ae8f8e936447eeb56190aa6aadc89d9901ce6114
John Aivaliotis <johnaivaliotis@protonmail.com>
2019-08-16 16:54:20 +1000

    Update const.ts
    Quick fix for domain changes.

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 export const apiUrl = process.env.NODE_ENV === 'development' ?
     'http://localhost:8000' :
-    'https://engine.panic.express'
+    'https://multiapi.bigheck.com'
 export const wsUrl = process.env.NODE_ENV === 'development' ?
     'ws://localhost:8000/api/ws' :
-    'wss://engine.panic.express/api/ws'
+    'wss://multiapi.bigheck.com/api/ws'
